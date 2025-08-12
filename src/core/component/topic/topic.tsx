fd30f41b2bdb3e37a488e0eb733a50147224fd47
Priyanshu Verma <priyanshuvr1308@gmail.com>
2025-08-12 19:01:42 +0530

    title font size change

--- src/core/component/topic/topic.tsx
+++ src/core/component/topic/topic.tsx
@@ -1,6 +1,6 @@
 export default function Topic({title}: TopProps) {
   return(
-    <div className="text-[35px] font-bold text-red-900 text-center my-5">
+    <div className="text-[24px] sm:text-[34px] font-bold text-red-900 text-center my-5">
         {title}
     </div>
   )
