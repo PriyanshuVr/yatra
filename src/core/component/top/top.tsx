184fc22cdac98330f0f4b94a66a696555d79ca4c
Priyanshu Verma <priyanshuvr1308@gmail.com>
2025-08-16 09:21:39 +0530

    top to home link

--- src/core/component/top/top.tsx
+++ src/core/component/top/top.tsx
@@ -4,7 +4,7 @@ export default function Top({version}: topProps) {
   const bgcolor = version === "v1" ? "bg-[#ff6201]" : version === "v2" ? "bg-[#fff0e3]" : "bg-white";
   return (
     <div className={`w-full relative flex items-center justify-center ${bgcolor}`}>
-      <Link href={"#"} className="m-1.5">
+      <Link href={"/"} className="m-1.5">
         <Image src={"https://yatra.brajesh.info/wp-content/uploads/2025/05/logo_yatra.png"} alt={""} width={80} height={80}></Image>
       </Link>
     </div>
